f3a471f575fa8b2d6161a6bdda43685eba9cc354
Bryce McDonnell <bryce@bridgetownint.com>
2015-07-31 09:15:41 -0400

    adds directives, accept/reject links

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,58 @@
 var elFrontend = angular.module("elFrontend", []);
 
+elFrontend.directive('acceptLink', function(Article) {
+  return {
+    restrict: "A",
+    scope : {
+      articleId : "@",
+      addOrReject : "@"
+    },
+    link: function(scope, elem, attrs) {
+      elem.bind('click', function(evt) {
+        evt.preventDefault();
+
+        var articleFunction;
+        if(scope.addOrReject == 'add') {
+          articleFunction = Article.addToReadingList;
+        } else {
+          articleFunction = Article.rejectFromReadingList;
+        }
+
+        articleFunction(scope.articleId).then(
+          //success
+          function(resp) {
+          $(elem).parent(".article").remove();
+          console.log("article update", resp.data);
+          },
+          // failure
+          function(data) {
+            console.warn("article update", data);
+          }
+        );
+      });
+    }
+  };
+});
+
 elFrontend.factory('Article', function($http) {
+  var host = "http://home.bam:4001";
   var allUndecided = function() {
     //return $http.get("https://email-listicle.herokuapp.com/api/v1/email_links/all");
-    return $http.get("http://home.bam:4001/api/v1/email_links/all");
+    return $http.get(host + "/api/v1/email_links/all");
+  };
+
+  var addToReadingList = function(id) {
+    return $http.post(host + "/api/v1/email_links/mark_for_read", {id: id});
+  };
+
+  var rejectFromReadingList = function(id) {
+    return $http.post(host + "/api/v1/email_links/mark_for_reject", {id: id});
   };
 
   return {
-    allUndecided : allUndecided
+    allUndecided : allUndecided,
+    addToReadingList : addToReadingList,
+    rejectFromReadingList : rejectFromReadingList
   };
 });
 
