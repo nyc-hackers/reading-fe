644b18bd0fbae61d95e4da115251e668f9a04703
Bryce McDonnell <bryce@bridgetownint.com>
2015-09-26 13:29:25 -0400

    not a great solution, but compiling a "production" build

--- public/js/app.js
+++ public/js/app.js
@@ -58,9 +58,7 @@ elFrontend.config(function($stateProvider, $urlRouterProvider,
   });
 });
 ;elFrontend.constant("Backend", {
-  //host: "http://home.bam:4001",
-  //host: "http://10.1.2.244:4001",
-  host: "http://readingapi.thedevranch.net:4001",
+  host: "https://email-listicle.herokuapp.com/"
 });
 ;elFrontend.factory("Article", function($http, Backend) {
   var host = Backend.host;
