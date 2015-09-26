bd61c16b1db8666cfae9ef28ee63569216ea04eb
Bryce McDonnell <bryce@bridgetownint.com>
2015-09-26 13:43:34 -0400

    routing backend to IP on dev environment

--- js/config/development.js
+++ js/config/development.js
@@ -1,5 +1,3 @@
 elFrontend.constant("Backend", {
-  //host: "http://home.bam:4001",
-  //host: "http://10.1.2.244:4001",
-  host: "http://readingapi.thedevranch.net:4001",
+  host: "http://10.1.2.244:4001",
 });
--- public/js/app.js
+++ public/js/app.js
@@ -58,7 +58,7 @@ elFrontend.config(function($stateProvider, $urlRouterProvider,
   });
 });
 ;elFrontend.constant("Backend", {
-  host: "https://email-listicle.herokuapp.com/"
+  host: "http://10.1.2.244:4001",
 });
 ;elFrontend.factory("Article", function($http, Backend) {
   var host = Backend.host;
