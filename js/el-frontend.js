f1dc349cbc3a63020b705e41af226584f11d981e
cassidypignatello <cassidy.pignatello@gmail.com>
2015-10-24 19:58:40 -0400

    fixes some satellizer config issues, includes it through cdn rather than npm install. adds google client secret to gitignore for now

--- js/el-frontend.js
+++ js/el-frontend.js
@@ -1,7 +1,7 @@
-var elFrontend = angular.module("elFrontend", ["ui.router"]);
+var elFrontend = angular.module("elFrontend", ["ui.router", "satellizer"]);
 
 elFrontend.config(function($stateProvider, $urlRouterProvider,
-                           $locationProvider) {
+                           $locationProvider, $authProvider) {
   // For any unmatched url, redirect to /state1
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise("/");
@@ -60,6 +60,14 @@ elFrontend.config(function($stateProvider, $urlRouterProvider,
   .state("login", {
     url: "/login",
     templateUrl: "/views/_login.html",
-    controller: function($scope) {}
+    controller: 'LoginCtrl'
+  });
+
+  $authProvider.google({
+    clientId: '1056669470112-m15b11qnmjcbus1t72obg56in98m8uf8.apps.googleusercontent.com'
+  });
+
+  $authProvider.github({
+    clientId: 'GitHub Client ID'
   });
 });
