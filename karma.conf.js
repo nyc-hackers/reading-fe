b58e0b32dbc5465cb6ecb2ebb6d1925263bb2401
cassidypignatello <cassidy.pignatello@gmail.com>
2015-10-25 20:38:48 -0400

    adds project files to karma config and creates working example test

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,9 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+        'node_modules/angular/angular.js',
+        'public/js/app.js',
+        'tests/*.js'
     ],
 
 
---
+++ tests/example.js
@@ -0,0 +1,5 @@
+describe('Example test', function () {
+  it('should be true', function() {
+    expect('foo').toBe('foo');
+  });
+})
